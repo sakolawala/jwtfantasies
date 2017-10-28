eead71bbc6fde023c9bb5cdd1fe5cd23d0635b4f
sakolawala <sakolawala@webuy.com>
2017-10-28 14:13:09 +0530

    Added methods for getting JWT token and calling the service using that token

--- clientmvc/Controllers/HomeController.cs
+++ clientmvc/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using clientmvc.Models;
+using RestSharp;
 
 namespace clientmvc.Controllers
 {
@@ -13,6 +14,29 @@ namespace clientmvc.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public JsonResult GetOAuthToken(string url)
+        {
+            var client = new RestClient(url);
+            var request = new RestRequest("connect/token", Method.POST);
+            request.AddHeader("x-api-key", "03ed12b14712499083d5aca24f56aed7");
+            IRestResponse response = client.ExecuteAsync2(request).Result;
+            var token = response.Content;
+            return Json(token);
+        }
+
+        [HttpGet]
+        public JsonResult GetDataFromService(string url, string token)
+        {
+            var client = new RestClient(url);
+            var request = new RestRequest(Method.GET);
+            var bearertoken = "Bearer " + token;
+            request.AddHeader("Authorization", bearertoken);
+            IRestResponse response = client.ExecuteAsync2(request).Result;
+            var data = response.Content;
+            return Json(data);
         }
 
         public IActionResult About()
@@ -32,6 +56,17 @@ namespace clientmvc.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        
+    }
+
+    public static class RestClientExtensions
+    {
+        public static async Task<RestResponse> ExecuteAsync2(this RestClient client, RestRequest request)
+        {
+            TaskCompletionSource<IRestResponse> taskCompletion = new TaskCompletionSource<IRestResponse>();
+            RestRequestAsyncHandle handle = client.ExecuteAsync(request, r => taskCompletion.SetResult(r));
+            return (RestResponse)(await taskCompletion.Task);
         }
     }
 }
