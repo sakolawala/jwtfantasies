c838c61cf2db7b7abe9ed7e2fe603f9eb505725d
sakolawala <sakolawala@webuy.com>
2017-10-30 12:16:11 +0530

    Added the Authentication Code

--- api1/Controllers/ValuesController.cs
+++ api1/Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 namespace api1.Controllers
 {
@@ -11,6 +12,7 @@ namespace api1.Controllers
     {
         // GET api/values
         [HttpGet]
+        [Authorize]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
--- api1/Startup.cs
+++ api1/Startup.cs
@@ -8,6 +8,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http;
+using System.Diagnostics;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Cryptography.X509Certificates;
 
 namespace api1
 {
@@ -23,7 +28,54 @@ namespace api1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string aud = Configuration.GetSection("jwt:Audience").Value;
+            string aut = Configuration.GetSection("jwt:Authority").Value;
+            bool requireHttps = Configuration.GetValue<bool>("jwt:RequireHttps");
+
+            services.AddAuthentication(option =>
+            {
+                option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(o =>
+            {
+                ConfigureWithOfflineValidation(o, aud);
+            });
             services.AddMvc();
+        }
+
+        private void ConfigureWithOfflineValidation(JwtBearerOptions o, string aud)
+        {
+            string certificatePath = Configuration.GetSection("jwt:CertificatePath").Value;
+            string certificatePassword = Configuration.GetSection("jwt:CertificatePassword").Value;
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                ValidateIssuer = true,
+                ValidIssuer = "http://10.4.1.232/kiwiservices/oauthservice",
+                IssuerSigningKey = new X509SecurityKey(new X509Certificate2(certificatePath, certificatePassword)),
+            };
+
+            o.Audience = aud;
+            o.TokenValidationParameters = tokenValidationParameters;
+        }
+
+        private void ConfigureWithOnlineValidation
+            (JwtBearerOptions o, string aud, string aut, bool requireHttps)
+        {
+            o.Audience = aud;
+            o.Authority = aut;
+            o.RequireHttpsMetadata = requireHttps;
+            o.Events = new JwtBearerEvents()
+            {
+                OnAuthenticationFailed = c =>
+                {
+                    Debug.WriteLine("OnAuthenticationFailed");
+                    c.NoResult();
+                    c.Response.StatusCode = 401;
+                    return c.Response.WriteAsync("Invalid Token or No Token");
+                }
+            };
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,6 +85,7 @@ namespace api1
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseAuthentication();
 
             app.UseMvc();
         }
