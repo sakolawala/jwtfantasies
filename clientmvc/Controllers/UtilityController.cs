1915f147014118212377e3f3fe73d6fb889441ef
sakolawala <sakolawala@webuy.com>
2017-11-01 16:02:15 +0530

    Added the controller to hash the password

---
+++ clientmvc/Controllers/UtilityController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace clientmvc.Controllers
+{
+    public class UtilityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetSHA256(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return Json(new { error = "Empty Input" });
+            }
+            else
+            {
+                using (var sha = SHA256.Create())
+                {
+                    var bytes = Encoding.UTF8.GetBytes(input);
+                    var hash = sha.ComputeHash(bytes);
+
+                    return Json(Convert.ToBase64String(hash));
+
+                }
+            }
+        }
+    }
+}
